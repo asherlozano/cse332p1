244ee5ba4a22fc6eef9a07203b6b99aa64fa4d49
catamtz3 <catamtz3@cs.washington.edu>
2022-04-04 20:23:57 -0700

    FIFO list operations FIXED

--- src/main/java/cse332/interfaces/worklists/FIFOWorkList.java
+++ src/main/java/cse332/interfaces/worklists/FIFOWorkList.java
@@ -8,36 +8,6 @@ package cse332.interfaces.worklists;
  * @param <E> the type of elements in the worklist
  * @author Adam Blank
  */
-public abstract class FIFOWorkList<E> extends WorkList<Void> {
-    private class Node{
-        int data;
-        Node next;
-        public Node (int data){
-            this.data = data;
-            this.next = null;
-        }
-    }
-    private Node front;
-    private Node rear;
-    public void Queue(){
-        this.front = this.rear = null;
-    }
-    public void enqueue(int data){
-        Node temp = new Node(data);
-        if (this.front == null){
-            this.front = this.rear = temp;
-        }
-        this.rear.next = temp;
-        this.rear = temp;
-    }
-    public void dequeue(){
-        if (this.front == null){
-            return;
-        }
-        Node temp = this.front;
-        this.front = this.front.next;
-        if(this.front == null){
-            this.rear = null;
-        }
-    }
+public abstract class FIFOWorkList<E> extends WorkList<E> {
+
 }
