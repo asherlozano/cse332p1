865f20fe200338f1282272badfc6a171403a87ab
asher <lozanoa@cs.washington.edu>
2022-04-05 16:35:19 -0700

    arrayStack finished

--- src/main/java/datastructures/worklists/ArrayStack.java
+++ src/main/java/datastructures/worklists/ArrayStack.java
@@ -3,38 +3,61 @@ package datastructures.worklists;
 import cse332.exceptions.NotYetImplementedException;
 import cse332.interfaces.worklists.LIFOWorkList;
 
+import java.util.NoSuchElementException;
+
 /**
  * See cse332/interfaces/worklists/LIFOWorkList.java
  * for method specifications.
  */
 public class ArrayStack<E> extends LIFOWorkList<E> {
-
+    private final int DEFAULT = 10;
+    private E[] arrayStack;
+    private int size;
     public ArrayStack() {
-        throw new NotYetImplementedException();
+        arrayStack = (E[]) new Object[DEFAULT];
+        size = 0;
     }
 
     @Override
     public void add(E work) {
-        throw new NotYetImplementedException();
+        if (size == arrayStack.length){
+            E[] tempArray = (E[]) new Object[arrayStack.length * 2];
+            for(int i = 0; i < arrayStack.length; i++){
+                tempArray E[i] = arrayStack E[i];
+            }
+            arrayStack = tempArray;
+        }
+        arrayStack[size] = work;
+        size++;
     }
 
     @Override
     public E peek() {
-        throw new NotYetImplementedException();
+        if (size == 0) {
+            throw new NoSuchElementException();
+        }
+        return arrayStack[size - 1];
+
+
     }
 
     @Override
     public E next() {
-        throw new NotYetImplementedException();
+        if (size == 0) {
+            throw new NoSuchElementException();
+        }
+        E nextElement = arrayStack[size - 1];
+        size--;
+        return nextElement;
     }
 
     @Override
     public int size() {
-        throw new NotYetImplementedException();
+        return size;
     }
 
     @Override
     public void clear() {
-        throw new NotYetImplementedException();
+        size = 0;
     }
 }
