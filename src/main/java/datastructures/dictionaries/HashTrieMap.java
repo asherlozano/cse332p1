23043f64b4572fc58ec1a20e8684bedeccba5ce5
catamtz3 <catamtz3@cs.washington.edu>
2022-04-13 21:01:36 -0700

    missing stuff

--- src/main/java/datastructures/dictionaries/HashTrieMap.java
+++ src/main/java/datastructures/dictionaries/HashTrieMap.java
@@ -108,7 +108,6 @@ public class HashTrieMap<A extends Comparable<A>, K extends BString<A>, V> exten
                     if (temp.pointers.size() > 1 || temp.value != null) {
                         multiChild = temp;
                         holderChild = findKey;
-                        this.size--;
                     }
                 }
                 temp = temp.pointers.get(findKey);
