a2d3cf3e979297481c80fffb37d1579dcab2bdbe
catamtz3 <catamtz3@cs.washington.edu>
2022-04-13 21:12:36 -0700

    missing stuff

--- src/main/java/datastructures/dictionaries/HashTrieMap.java
+++ src/main/java/datastructures/dictionaries/HashTrieMap.java
@@ -48,12 +48,10 @@ public class HashTrieMap<A extends Comparable<A>, K extends BString<A>, V> exten
             }
             front = front.pointers.get(search);
         }
-        V returnVal = front.value;
         if(front.value == null){
             size++;
         }
-        front.value = value;
-        return returnVal;
+        return front.value;
     }
 
     @Override
